8d0e1d59c4d4df94cec3c46cbc46915c9bc2a704
GeorgeOfGithub <lucasbuhelt@gmail.com>
2022-10-26 16:31:34 +0200

    Start

--- Project1.py
+++ Project1.py
@@ -1,43 +1,9 @@
 import numpy as np
 import pandas as pd
+import inputNumber
+import displayMenu
 
 #global data
-
-
-
-def inputNumber(prompt):
-# INPUTNUMBER Prompts user to input a number
-#
-# Usage: num = inputNumber(prompt) Displays prompt and asks user to input a
-# number. Repeats until user inputs a valid number.
-#
-# Author: Mikkel N. Schmidt, mnsc@dtu.dk, 2015
-    while True:
-        try:
-            num = float(input(prompt))  
-            break
-        except ValueError:
-            pass
-    return num
-
-def displayMenu(options):
-# DISPLAYMENU Displays a menu of options, ask the user to choose an item
-# and returns the number of the menu item chosen.
-#
-# Usage: choice = displayMenu(options)
-#
-# Input options Menu options (array of strings)
-# Output choice Chosen option (integer)
-#
-# Author: Mikkel N. Schmidt, mnsc@dtu.dk, 2015
-# Display menu options
-    for i in range(len(options)):
-        print("{:d}. {:s}".format(i+1, options[i]))
-    # Get a valid menu choice
-    choice = 0
-    while not(np.any(choice == np.arange(len(options))+1)):
-        choice = inputNumber("Please choose a menu item: ")
-    return choice
 
 
 def load_data(dataname):
